f30c33717781fa61569518cd76e4c87d2a9cd8d1
Josh Broughton <spikebroughton@aol.com>
2021-03-11 21:22:21 -0500

    employee

---
+++ components/Head.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on object to filter through by headings, or use the search bar</p>
+            </div>
+    )
+}
+export default Header;
