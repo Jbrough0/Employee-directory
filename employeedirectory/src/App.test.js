f8236b8a8a480ee92d8bd1ce14781543dae88c8a
Josh Broughton <spikebroughton@aol.com>
2021-03-15 15:16:32 -0400

    add

--- employeedirectory/src/App.test.js
+++ employeedirectory/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('run app without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode (div);
 });
